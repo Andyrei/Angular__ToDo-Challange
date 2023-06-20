1d86f6abbb7cb4546a37d2f3bfce706a6ad2f8f9
Andyrei <106072367+Andyrei@users.noreply.github.com>
2023-06-20 02:47:52 +0200

    Added typewriting effect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgxTypedJsModule} from 'ngx-typed-js';
 
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AppComponent } from 'src/app/app.component';
 import { TodosModule } from 'src/app/todos/todos.module';
-import { MainComponent } from './todos/components/main/main.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IntroComponent } from './intro/intro/intro.component';
 
@@ -19,7 +19,8 @@ import { IntroComponent } from './intro/intro/intro.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     TodosModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    NgxTypedJsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
